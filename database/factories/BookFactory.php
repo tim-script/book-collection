bd431ea629838874fdcee06ecb99ead056351371
Tim <tim-script@posteo.com>
2024-11-20 12:36:03 +0100

    Add models, migrations, factories and seeders

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'author_id' => Author::inRandomOrder()->first()->id,
+            'title' => ucfirst($this->faker->words(rand(1, 5), true)),
+        ];
+    }
+}
---
+++ database/migrations/2024_11_20_104022_create_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Author::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('review')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+        Schema::dropIfExists('authors');
+    }
+};

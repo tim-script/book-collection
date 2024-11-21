1a85c6be9e1ef83a53764275a1ee7e8ab7b48ea3
Tim <tim-script@posteo.com>
2024-11-21 15:28:37 +0100

    Get authors from API

--- resources/js/domains/books/pages/Overview.vue
+++ resources/js/domains/books/pages/Overview.vue
@@ -1,13 +1,18 @@
 <script setup>
     import { computed } from 'vue'
-    import { fetchBooks, getAllBooks } from '../store'
+    import { fetchAuthors, fetchBooks, getAllAuthors, getAllBooks } from '../store'
 
+    fetchAuthors()
     fetchBooks()
 
+    const authors = computed(() => getAllAuthors())
     const books = computed(() => getAllBooks())
 </script>
 
 <template>
+    <ul>
+        <li v-for="author in authors.data">{{author}}</li>
+    </ul>
     <ul>
         <li v-for="book in books.data">{{book}}</li>
     </ul>
--- resources/js/domains/books/store.js
+++ resources/js/domains/books/store.js
@@ -1,5 +1,16 @@
 import { ref } from 'vue'
 
+const authors = ref([])
+
+export const fetchAuthors = async () => {
+    const {data} = await axios.get('api/authors')
+    if (!data)
+        return
+    authors.value = data
+}
+
+export const getAllAuthors = () => authors.value
+
 const books = ref([])
 
 export const fetchBooks = async () => {

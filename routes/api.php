9d8020e5a548bea6f761fa4cf771ba54889725a4
Tim <tim-script@posteo.com>
2024-11-21 15:17:27 +0100

    Add book API
    php artisan make:controller -m Book BookController
    php artisan make:resource BookResource

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/books', [BookController::class, 'index']);
